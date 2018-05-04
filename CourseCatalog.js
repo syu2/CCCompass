3f8cceb76fc3fbf4adb24c28d59e9e0435c2fdf9
syu2 <36242597+syu2@users.noreply.github.com>
2018-05-04 09:12:50 -0400

    Update CourseCatalog.js

--- CourseCatalog.js
+++ CourseCatalog.js
@@ -27,9 +27,7 @@ course.loadCollapsible = function(){
   // var smallID = CourseID.slice(0,10);
 
   // smallID.forEach(function(id){
-  CourseID.forEach(function(id){
-    console.log(CourseID.length);
-    
+  CourseID.forEach(function(id){    
       var data = Course["Course " + id];
 
         data.forEach(function( a ){
